e4a521c2314e0f3fe4c4758e783fb9929b5761f7
josep <josepedroaraujo@outlook.pt>
2024-11-23 22:26:31 +0100

    dhfjd

---
+++ home.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Selecionar o carrossel do Bootstrap
+    const testimonialCarousel = new bootstrap.Carousel("#testimonialCarousel");
+  
+    // Botões de controle
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+  
+    // Adicionar eventos de clique
+    prevBtn.addEventListener("click", function () {
+      testimonialCarousel.prev(); // Vai para o slide anterior
+    });
+  
+    nextBtn.addEventListener("click", function () {
+      testimonialCarousel.next(); // Vai para o próximo slide
+    });
+  });
+  
