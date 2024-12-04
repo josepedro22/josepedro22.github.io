956583e8097f3f4593422f7d014dc7a8e28dcdb8
josep <josepedroaraujo@outlook.pt>
2024-12-04 20:09:38 +0100

    1234

---
+++ flavours_prices.js
@@ -0,0 +1,4 @@
+// Personalização de modais ou comportamento adicional
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Página carregada. Modais prontos para interação.");
+});
